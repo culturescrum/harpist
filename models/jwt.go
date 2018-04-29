b0214d6da6be936e713d0be0380d5c70cf660521
Michael King <mking@arikel.net>
2018-04-29 15:27:44 -0400

    initial models

---
+++ models/jwt.go
@@ -0,0 +1,6 @@
+package models
+
+// JwtToken defines JWT Token structure
+type JwtToken struct {
+  Token string `json:"token"`
+}
---
+++ models/user.go
@@ -0,0 +1,7 @@
+package models
+
+// User defines basic user structure
+type User struct {
+  Username string `json:"username"`
+  Password string `json:"password"`
+}
