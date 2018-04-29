6e35d3b21549f09188de37823e4a2e681fb65eeb
Michael King <mking@arikel.net>
2018-04-29 16:49:14 -0400

    refactor to use interfaces for later iteration

--- models/group.go
+++ models/group.go
@@ -1,30 +1,84 @@
 package models
 
-// Group contains the fields for inheritance in other group types
-type Group struct {
-  ID            int64         `json:"id"`
-  Name          string        `json:"name"`
+// Group creates the generic interface for groups and authorization
+type Group interface {
+  owner(User)               bool
+  isMember(User)            bool
 }
 
-// TemporalGroup defines fields for a centralized group owned by a
-// single user
+// TemporalGroup defines the fields for generic groups
 type TemporalGroup struct {
-  Group
-  Owner         User          `json:"owner"`
+  ID            int64         `json:"id"`
+  Name          string        `json:"name"`
+  Creator       User          `json:"creator"`
+  Admins        []User        `json:"-"`
   Members       []User        `json:"-"`
+}
+
+func (tg TemporalGroup) owner(user User) bool {
+  return tg.Creator == user
+}
+
+func (tg TemporalGroup) isMember(user User) bool {
+  for _, member := range tg.Members {
+    if user == member {
+      return true
+    }
+    // This will probably be controversial and up for discussion
+    if user == tg.Creator {
+      return true
+    }
+  }
+  return false
 }
 
 // Conglomerate defines fields for an umbrella group owned by a group of
 // users, mainly used to manage ConglomerateGroup structures.
 type Conglomerate struct {
-  Group
-  Owners        []User        `json:"-"`
+  TemporalGroup
+  Directors     []User                `json:"-"`
+  Members       []ConglomerateGroup   `json:"-"`
+}
+
+func (c Conglomerate) owner(user User) bool {
+  for _, director := range c.Directors {
+    if user == director {
+      return true
+    }
+  }
+  return false
+}
+
+func (c Conglomerate) isMember(user User) bool {
+  for _, cg := range c.Members {
+    if cg.isMember(user) {
+      return true
+    }
+  }
+  if c.owner(user) {
+    return true
+  }
+  return false
 }
 
 // ConglomerateGroup defines fields for groups within an umbrella group
 // with ownership passed to a Conglomerate
 type ConglomerateGroup struct {
-  Group
+  TemporalGroup
+  GroupName     string        `json:"group_name"`
   Owner         Conglomerate  `json:"-"`
-  Admins        []User        `json:"-"`
+}
+
+func (cg ConglomerateGroup) owner(user User) bool {
+  for _, director := range cg.Owner.Directors {
+    if user == director {
+      return true
+    }
+  }
+
+  if cg.Creator == user {
+    return true
+  }
+
+  return false
 }
