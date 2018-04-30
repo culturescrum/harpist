18cf0f266f5a37a15e7aca2a8949a06d6557aa56
Michael King <mking@arikel.net>
2018-04-30 19:27:18 -0400

    initial auth internals

---
+++ internal/auth/jwt.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+  // "encoding/json"
+  // "net/http"
+  // "strings"
+  // "fmt"
+  // "time"
+  // "context"
+
+  "github.com/culturescrum/harpist/models"
+
+  "github.com/dgrijalva/jwt-go"
+)
+
+// UserClaim is for web interactions with the API and authentication
+type UserClaim struct {
+  *jwt.StandardClaims
+  TokenType string
+  models.User
+}
