24d71eaaffb903da22fcbb390221ec2093db83a8
Michael King <mking@arikel.net>
2018-04-30 19:15:51 -0400

    added yaml tags

--- models/user.go
+++ models/user.go
@@ -2,7 +2,7 @@ package models
 
 // User defines basic user structure
 type User struct {
-  ID int64 `json:"id"`
-  Username string `json:"username"`
-  Password string `json:"password"`
+  ID int64 `json:"id" yaml:"user_id"`
+  Username string `json:"username" yaml:"username"`
+  Password string `json:"password" yaml:"password"`
 }
