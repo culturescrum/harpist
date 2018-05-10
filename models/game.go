e2ab72e9a62cdaa0b5606c2c11375e8766422a9d
Michael King <mking@arikel.net>
2018-05-09 20:40:12 -0400

    added character and game models for gorm

--- models/game.go
+++ models/game.go
@@ -6,17 +6,21 @@ import (
 
 // GameInfo implements base Game information used by interfaces
 type GameInfo struct {
-	ID    uint   `json:"id"`
-	Name  string `json:"name"`
-	Owner User
+	ID      uint   `json:"id" gorm:"primary_key"`
+	Name    string `json:"name"`
+	Owner   User   `json:"owner" gorm:"foreignkey:OwnerID"`
+	OwnerID uint   `json:"-"`
 }
 
 // Game implements
 type Game struct {
-	GameInfo
-	Characters []Character
-	Visitors   []Character
-	GameAdmins []User
+	ID         uint        `json:"id" gorm:"primary_key"`
+	Name       string      `json:"name"`
+	Owner      User        `json:"owner" gorm:"foreignkey:OwnerID"`
+	OwnerID    uint        `json:"-"`
+	Characters []Character `json:"characters" gorm:"many2many:game_characters"`
+	Players    []User      `json:"players" gorm:"many2many:game_players"`
+	GameAdmins []User      `json:"admins" gorm:"many2many:game_admins"`
 }
 
 func (g Game) HarpistType() HarpistType {
@@ -34,16 +38,11 @@ func (g Game) isPlayer(c Character) bool {
 			return true
 		}
 	}
-	for _, ch := range g.Visitors {
-		if c == ch {
-			return true
-		}
-	}
 	return false
 }
 
 func (g Game) GroupMembers() []Character {
-	return append(g.Characters, g.Visitors...)
+	return g.Characters
 }
 
 func (g Game) GroupAdmins() []User {
@@ -62,8 +61,74 @@ func (g Game) MemberIdentity() uint {
 	return g.ID
 }
 
+func (g *Game) AddAdmin(u User) error {
+	for _, user := range g.GameAdmins {
+		if user == u {
+			return nil // Nothing to do, they're already there
+		}
+	}
+
+	g.GameAdmins = append(g.GameAdmins, u)
+	return nil
+}
+
+func (g *Game) RemoveAdmin(u User) error {
+	for i, user := range g.GameAdmins {
+		if user == u {
+			g.GameAdmins = append(g.GameAdmins[:i], g.GameAdmins[i+1:]...)
+			return nil
+		}
+	}
+	return nil // nothing to do
+}
+
+func (g *Game) AddMember(u User) error {
+	for _, user := range g.Players {
+		if user == u {
+			return nil // Nothing to do, they're already there
+		}
+	}
+
+	g.Players = append(g.Players, u)
+	return nil
+}
+
+func (g *Game) RemoveMember(u User) error {
+	for i, user := range g.Players {
+		if user == u {
+			g.Players = append(g.Players[:i], g.Players[i+1:]...)
+			return nil
+		}
+	}
+	return nil // nothing to do
+}
+
+func (g *Game) AddCharacter(c Character) error {
+	for _, char := range g.Characters {
+		if char == c {
+			return nil // Nothing to do, they're already there
+		}
+	}
+
+	g.Characters = append(g.Characters, c)
+	return nil
+}
+
+func (g *Game) RemoveCharacter(c Character) error {
+	for i, char := range g.Characters {
+		if char == c {
+			g.Characters = append(g.Characters[:i], g.Characters[i+1:]...)
+			return nil
+		}
+	}
+	return nil // nothing to do
+}
+
 type Setting struct {
-	GameInfo
+	ID         uint   `json:"id" gorm:"primary_key"`
+	Name       string `json:"name"`
+	Owner      User   `json:"owner" gorm:"foreignkey:OwnerID"`
+	OwnerID    uint   `json:"-"`
 	Games      []Game
 	GameAdmins []User
 }
@@ -114,7 +179,10 @@ func (gs GameSession) Info() Group {
 }
 
 type Character struct {
-	GameInfo
+	ID      uint   `json:"id" gorm:"primary_key"`
+	Name    string `json:"name"`
+	Owner   User   `json:"owner" gorm:"foreignkey:OwnerID"`
+	OwnerID uint   `json:"-"`
 }
 
 func (c Character) HarpistType() HarpistType {
