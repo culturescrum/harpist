03d7ef919b43a0765312dfb86b8b05f832ec2cda
Michael King <mking@arikel.net>
2018-05-28 17:13:58 -0400

    initial tests

---
+++ harpist_test.go
@@ -0,0 +1,47 @@
+package harpist
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+)
+
+// TestInit tests database initialization
+func TestInit(t *testing.T) {
+	var config interface{} = HarpistConfig
+	var db interface{} = HarpistDB.DB()
+	var logger interface{} = HarpistLogger
+
+	cv, ok := config.(Config)
+	if !ok {
+		t.Error(
+			"For", config,
+			"expected Config, got", cv,
+		)
+	}
+
+	dv, ok := db.(*sql.DB)
+	if !ok {
+		t.Error(
+			"For", db,
+			"expected sql.DB, got", dv,
+		)
+
+	}
+
+	lv, ok := logger.(*log.Logger)
+
+	if !ok {
+		t.Error(
+			"For", logger,
+			"expected log.Logger, got", lv,
+		)
+	}
+
+	_, lcheck := os.Stat("harpist.dev.log")
+	if os.IsNotExist(lcheck) {
+		t.Errorf("Logger did not initialize new log file")
+	}
+
+}
