3dbc86a62bc717d1bc8084fb9b8d26f0ccc7359c
Michael King <mking@arikel.net>
2018-05-28 12:54:51 -0400

    oops.

--- models/main.go
+++ models/main.go
@@ -1,8 +1,10 @@
 package models
 
+// CoreModels provides a slice of models to migrate when initializing the api.
+var CoreModels []interface{}
+
 func init() {
-	// CoreModels provides a slice of models to migrate when initializing the api.
-	CoreModels := make([]interface{}, 4)
+	CoreModels = make([]interface{}, 4)
 	CoreModels[0] = &User{}
 	CoreModels[1] = &PlayGroup{}
 	CoreModels[2] = &Game{}
